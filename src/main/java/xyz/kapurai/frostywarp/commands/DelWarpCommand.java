56301daede34788778411bb4b1eb1b1acac0c55a
lekro <kapuraya@gmail.com>
2019-06-22 13:00:48 -0500

    Add GPLv3 license

--- src/main/java/xyz/kapurai/frostywarp/commands/DelWarpCommand.java
+++ src/main/java/xyz/kapurai/frostywarp/commands/DelWarpCommand.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 lekro (kapurai).
+ *
+ * This file is part of frostywarp.
+ *
+ * frostywarp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * frostywarp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with frostywarp.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 package xyz.kapurai.frostywarp.commands;
 
 import org.bukkit.command.Command;
